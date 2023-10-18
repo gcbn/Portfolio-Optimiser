cb37680d06180e26d8e686404455e0fc7fcb3894
gcbn <74546980+gcbn@users.noreply.github.com>
2023-10-18 18:18:17 +0100

    Portfolio Optimation Implemented with Monte Carlo Simulations and SciPy Optimise

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio-Optimiser
-Using MPT, Black-Litterman and Monte-Carlo Simulations to optimise portfolio stock selections
---
+++ sp500.py
@@ -0,0 +1,4 @@
+
+
+for i in ['AET', 'BMS', 'CA', 'COL', 'ESRX', 'HOT', 'NFX', 'SNI', 'TWX', 'AET', 'BMS', 'CA', 'COL', 'ESRX', 'HOT', 'NFX', 'SNI', 'TWX']:
+    print(i)
