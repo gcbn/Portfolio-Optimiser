2775d1b81bd6e2e1616e15e8e14aa8c6600eb845
gcbn <74546980+gcbn@users.noreply.github.com>
2024-10-09 23:13:12 +0100

    Removed Invalid Tickers and updated app.py for readability

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+
 from collections import defaultdict
 
 from flask import Flask, render_template, request
@@ -15,10 +16,7 @@ app = Flask(__name__)
 with open('tickers.txt') as my_file:
     symbols = my_file.readlines()
 symbols = literal_eval(symbols[0])
-
-# Small set to test new features
-# symbols = ["GOOGL", "META", "AMZN", "NFLX", "NVDA", "GS"]
-
+#symbols = ["GOOGL", "META", "AMZN", "NFLX", "NVDA", "GS"]
 start_date = datetime(2015, 1, 1)
 end_date = datetime.now()
 today = datetime.date(end_date)
@@ -27,23 +25,19 @@ SOURCE = 'yahoo'
 data = yf.download(symbols, start=start_date, end=end_date)
 data = data["Adj Close"]
 
-# For data presentation on initial page load
 rets, vars = util.stock_stats(data)
 sharpes = defaultdict(str)
 for s in symbols:
-    sharpes[s] = rets[s] / (vars[s] ** 0.5)
-
+    sharpes[s] = rets[s]/(vars[s]**0.5)
 
 # Define the root route
 @app.route('/')
 def index():
     return render_template('index.html', symbols=symbols, rets=rets, vars=vars, sharpes=sharpes, today=today)
 
-
 @app.route('/portfolio')
 def portfolio():
     return render_template('optimise.html', symbols=symbols, today=today)
-
 
 @app.route('/callback/<endpoint>')
 def cb(endpoint):
@@ -63,17 +57,17 @@ def cb(endpoint):
         weights = optimise.opt_weights(data1[syms])
         mean_rets, cov_mat = util.calc_returns_stats(data1[syms])
         p1r, p1v, p1d = util.portfolio_stats(mean_rets, cov_mat, np.array(weights[0]))
-        p1s = round((p1r / p1d) * (252 ** 0.5), 3)
-        p1r = round(p1r * 252 * 100, 2)
-        p1v = round(p1v * 252, 3)
+        p1s = round((p1r/p1d)*(252**0.5), 3)
+        p1r = round(p1r*252*100, 2)
+        p1v = round(p1v*252, 3)
         p2r, p2v, p2d = util.portfolio_stats(mean_rets, cov_mat, np.array(weights[1]))
-        p2s = round((p2r / p2d) * (252 ** 0.5), 3)
-        p2r = round(p2r * 252 * 100, 2)
-        p2v = round(p2v * 252, 3)
-        opt, mini = [], []
+        p2s = round((p2r/p2d)*(252**0.5), 3)
+        p2r = round(p2r*252*100, 2)
+        p2v = round(p2v*252, 3)
+        opt,mini = [], []
         for i in range(len(syms)):
-            opt.append([syms[i], str(round(weights[0][i] * 100, 2)) + "%"])
-            mini.append([syms[i], str(round(weights[1][i] * 100, 2)) + "%"])
+            opt.append([syms[i], str(round(weights[0][i]*100, 2))+"%"])
+            mini.append([syms[i], str(round(weights[1][i]*100, 2))+"%"])
 
         return [opt, p1r.item(), p1v.item(), p1s.item(), mini, p2r.item(), p2v.item(), p2s.item()]
     elif endpoint == "stockData":
@@ -84,9 +78,7 @@ def cb(endpoint):
         sharpes = defaultdict(str)
         for s in symbols:
             sharpes[s] = rets[s] / (vars[s] ** 0.5)
-        tableformat = [
-            [s, str(round(rets[s] * 100 * 252, 2)) + "%", round(vars[s] * 252, 3), round(sharpes[s] * 252 ** 0.5, 3)]
-            for s in symbols]
+        tableformat = [[s, str(round(rets[s]*100*252,2))+"%", round(vars[s]*252, 3), round(sharpes[s]*252**0.5, 3)] for s in symbols]
         return tableformat
     else:
         return "Bad endpoint", 400
